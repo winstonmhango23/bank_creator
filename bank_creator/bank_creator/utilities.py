a377dec71cda07fa19d24a1a0a0d1bda77f2002f
winstonmhango23 <winstonmhango23@gmail.com>
2024-07-25 03:59:59 +0200

    updated the Readme.md

--- bank_creator/bank_creator/utilities.py
+++ bank_creator/bank_creator/utilities.py
@@ -3,6 +3,8 @@ def transfer(from_account, to_account, amount):
     if from_account.balance >= amount:
         from_account.withdraw(amount)
         to_account.deposit(amount)
-        print(f"Transferred {amount} from {from_account.account_holder} to {to_account.account_holder}.")
+        print(
+            f"Transferred {amount} from {from_account.account_holder} to {to_account.account_holder}."
+        )
     else:
         print("Insufficient funds for transfer.")
